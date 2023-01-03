32144a0a3b6527afb366c85c24f827329568d079
hahsan1 <72406931+hahsan1@users.noreply.github.com>
2023-01-03 17:58:29 -0500

    Added basic file checks for each step in test-osiris script

--- tests/testthat/test-osiris.R
+++ tests/testthat/test-osiris.R
@@ -18,7 +18,7 @@ osiris::calculate_deltas_from_climate(
   write_dir = paste0(data_folder,"/outputs_calculate_delta_from_climate"),
   monthly_growing_season = paste0(data_folder,"/growing_seasons/pmonth_gslength_unifWheat_smallareamask.csv"),
   monthly_harvest_season = paste0(data_folder,"/growing_seasons/p_h_months_unifWheat_smallareamask.csv"),
-  growing_season_dir = paste0(data_folder,"/growing_season_climate_data"),
+  growing_season_dir = paste0(data_folder,"/outputs_growing_season_climate_data"),
   esm_name = "CanESM5",
   crops = c("Corn", "Wheat", "Rice", "Soy"),
   irrigation_rainfed = c("IRR", "RFD"),
@@ -42,3 +42,87 @@ test_that("calculate_deltas_from_climate produces expected files", {
                              "CanESM5_wheat_irr_smooth_deltaT_deltaP.csv","CanESM5_wheat_rfd_smooth_deltaT_deltaP.csv"))
 
 })
+
+# Step 2 - grid_to_basin_yield
+osiris::grid_to_basin_yield(
+  carbon = paste0(data_folder,"/yield_response_inputs/magicc_rcp8p5_co2.csv"),
+  weight_floor_ha = 1,
+  emulator_dir = paste0(data_folder,"/yield_response_fcns/ggcmi_phase2"),
+  input_dir = paste0(data_folder,"/outputs_calculate_delta_from_climate"),
+  area_dir = paste0(data_folder,"/area_data"),
+  basin_grid = paste0(data_folder,"/mapping_data/gridData.csv"),
+  basin_id = paste0(data_folder,"/mapping_data/gcam_basin_ids.csv"),
+  write_dir = paste0(data_folder,"/outputs_grid_to_basin_yield"),
+  wheat_area = paste0(data_folder,"/winter_and_spring_wheat_areas_v1_180627.nc4"),
+  crops = c("maize", "rice", "soy", "wheat"),
+  esm_name = "CanESM5",
+  cm_name = "LPJmL",
+  scn_name = "ssp245",
+  N = 200
+)
+
+test_that("grid_to_basin_yield produces expected files", {
+
+  testthat::expect_identical( unlist(list.files(paste0(data_folder,"/outputs_grid_to_basin_yield"))),
+                              c("LPJmL_CanESM5_ssp245_maize_2010_2020.csv",
+                                "LPJmL_CanESM5_ssp245_rice_2010_2020.csv",
+                                "LPJmL_CanESM5_ssp245_soy_2010_2020.csv",
+                                "LPJmL_CanESM5_ssp245_wheat_2010_2020.csv"))
+
+})
+
+# Step 3 - yield_to_gcam_basin
+osiris::yield_to_gcam_basin(
+  write_dir = paste0(data_folder,"/outputs_yield_to_gcam_basin"),
+  emulated_basin_yield_dir = paste0(data_folder,"/outputs_grid_to_basin_yield"),
+  iso_GCAM_region_mapping = paste0(data_folder,"/mapping_data/iso_GCAM_regID.csv"),
+  FAO_ag_mapping = paste0(data_folder,"/gcamdata_files/FAO_ag_items_PRODSTAT_expanded_corrected.csv"),
+  iso_harvest_area_mapping = paste0(data_folder,"/gcamdata_files/L100.LDS_ag_HA_ha.csv"),
+  iso_GCAM_basin_mapping = paste0(data_folder,"/mapping_data/gcam_basin_ids.csv"),
+  esm_name = "CanESM5",
+  scn_name = "ssp245",
+  max_CCImult = 2.5,
+  min_CCImult = 0.01,
+  weight_floor_ha = 1,
+  rolling_avg_years = 1,
+  maxHistYear = 2015,
+  minFutYear = 2015,
+  maxFutYear = 2020,
+  extrapolate_to = NULL
+)
+
+test_that("yield_to_gcam_basin produces expected files", {
+
+  testthat::expect_identical( unlist(list.files(paste0(data_folder,"/outputs_yield_to_gcam_basin"))),
+                              c("ag_impacts_CanESM5_ssp245_rcp_gcm_gcm_R_GLU_C_IRR_allyears_RA3_gridcull_allyroutlier.csv",
+                                "bio_impacts_CanESM5_ssp245_rcp_gcm_gcm_R_GLU_C_IRR_allyears_RA3_gridcull_allyroutlier.csv"))
+
+})
+
+# Step 4 - create_AgProdChange_xml
+osiris::create_AgProdChange_xml(
+  write_dir = paste0(data_folder,"/outputs_create_AgProdChange_xml"),
+  esm_name = 'CanESM5',
+  scn_name = 'ssp245',
+  ssp = 'ssp5',
+  ag_irr_ref = paste0(data_folder,"/reference_agprodchange/L2052.AgProdChange_irr_high.csv"),
+  bio_irr_ref = paste0(data_folder,"/reference_agprodchange/L2052.AgProdChange_bio_irr_ref.csv"),
+  ag_impacts = paste0(data_folder,"/outputs_yield_to_gcam_basin/ag_impacts_CanESM5_ssp245_rcp_gcm_gcm_R_GLU_C_IRR_allyears_RA3_gridcull_allyroutlier.csv"),
+  bio_impacts = paste0(data_folder,"/outputs_yield_to_gcam_basin/bio_impacts_CanESM5_ssp245_rcp_gcm_gcm_R_GLU_C_IRR_allyears_RA3_gridcull_allyroutlier.csv"),
+  GCAM_region_mapping = paste0(data_folder,"/mapping_data/GCAM_region_names.csv"),
+  timestep = 5,
+  maxHistYear = 2010,
+  minFutYear = 2015,
+  appliedto = "full"
+)
+
+test_that("create_AgProdChange_xml produces expected files", {
+
+  testthat::expect_identical( unlist(list.files(paste0(data_folder,"/outputs_create_AgProdChange_xml"))),
+                              c("ag_prodchange_ssp245_ssp5_CanESM5_LPJmL.csv",
+                                "ag_prodchange_ssp245_ssp5_CanESM5_LPJmL.xml",
+                                "ag_prodchange_ssp245_ssp5_CanESM5_NA.csv",
+                                "ag_prodchange_ssp245_ssp5_CanESM5_NA.xml"))
+
+})
+
